12edc7b6b91c28a69ddbbe55d5d58e7020e83d6e
leechangjoon <2018110021@dongguk.edu>
2024-02-08 21:24:48 +0900

    [Design] 구글 맵 들어갈 위치 조정

--- src/pages/mainpage/Mainpage.tsx
+++ src/pages/mainpage/Mainpage.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import * as S from "./style";
 import CardContainer from "../../components/mainpage/CardContainer/CardContainer";
 import Search from "../../components/mainpage/SearchContainer/Search";
 
 const Mainpage = () => {
   return (
-    <div>
-      <Search />
-      <CardContainer />
-    </div>
+    <S.MainWrapper>
+      <div>
+        <Search />
+        <CardContainer />
+      </div>
+      <div>구글맵 페이지</div>
+    </S.MainWrapper>
   );
 };
 
