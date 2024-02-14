42b0187aaddcd52716d7af375be66f4ed536cdf5
Hyeona01 <sha0209@dgu.ac.kr>
2024-02-14 16:29:14 +0900

    feat: 소셜 로그인 api 통신

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: process.env.VITE_APP_SERVER_HOST,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
--- src/components/login/login/SocialLogin.tsx
+++ src/components/login/login/SocialLogin.tsx
@@ -3,15 +3,46 @@ import * as S from "./style";
 import Google from "../../../assets/image/login/google_btn.png";
 import Kakao from "../../../assets/image/login/kakao_btn.png";
 import Naver from "../../../assets/image/login/naver_btn.png";
+import { apiClient } from "../../../api/ApiClient";
 
 const SocialLogin = () => {
+  // 구글 로그인
+  const fetchGoogleLogin = async () => {
+    try {
+      const response = await apiClient.get("api/v1/user/google/login"); //테스트 중
+      console.log(response.data.results);
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
+  // 카카오 로그인
+  const fetchKakaoLogin = async () => {
+    try {
+      const response = await apiClient.get("api/v1/user/kakao/login"); //테스트 중
+      console.log(response.data.results);
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
+  // 네이버 로그인
+  const fetchNaverLogin = async () => {
+    try {
+      const response = await apiClient.get("api/v1/user/naver/login"); //테스트 중
+      console.log(response.data.results);
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
   return (
     <S.SocialLoginWrapper>
       <S.SmallTxt>간편로그인</S.SmallTxt>
       <S.FlexRow>
-        <img src={Google} alt="구글" />
-        <img src={Kakao} alt="카카오" />
-        <img src={Naver} alt="네이버" />
+        <img src={Google} alt="구글" onClick={fetchGoogleLogin} />
+        <img src={Kakao} alt="카카오" onClick={fetchKakaoLogin} />
+        <img src={Naver} alt="네이버" onClick={fetchNaverLogin} />
       </S.FlexRow>
     </S.SocialLoginWrapper>
   );
