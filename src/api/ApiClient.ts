ccde9d499fda52ee99a2c3dffb880580b497507b
Hyeona01 <sha0209@dgu.ac.kr>
2024-02-14 17:22:25 +0900

    feat: baseURL 변경

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: process.env.VITE_APP_SERVER_HOST,
+  baseURL: "https://blink-dgu.com/",
+  // baseURL: process.env.VITE_APP_SERVER_HOST,
   headers: {
     "Content-Type": "application/json",
   },
