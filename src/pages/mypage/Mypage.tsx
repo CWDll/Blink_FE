17315475fcf62943668cacced85308b98116035f
Hyeona01 <sha0209@dgu.ac.kr>
2024-02-29 00:31:06 +0900

    [마이] 나의찾아요, 나의 제보해요 추가

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -3,6 +3,7 @@ import * as S from "./style";
 import MypageNav from "../../components/mypage/mypageNav/MypageNav";
 import MyProfile from "../../components/mypage/myProfile/MyProfile";
 import ChatList from "../../components/mypage/chatList/ChatList";
+import MyCard from "../../components/mypage/myCard/MyCard";
 
 const Mypage = () => {
   return (
@@ -12,6 +13,10 @@ const Mypage = () => {
         <S.DisplayFlex>
           <MyProfile />
           <ChatList />
+        </S.DisplayFlex>
+        <S.DisplayFlex>
+          <MyCard />
+          <MyCard />
         </S.DisplayFlex>
       </div>
       <div>최근 검색어</div>
