128ccb67d061c0ea8e5b5fca3bc29718d99d86cd
Hyeona01 <sha0209@dgu.ac.kr>
2024-02-29 17:34:53 +0900

    [마이] 배너 일부 추가

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -4,6 +4,7 @@ import MypageNav from "../../components/mypage/mypageNav/MypageNav";
 import MyProfile from "../../components/mypage/myProfile/MyProfile";
 import ChatList from "../../components/mypage/chatList/ChatList";
 import MyCard from "../../components/mypage/myCard/MyCard";
+import RightBanner from "../../components/layouts/banner/RightBanner";
 
 const Mypage = () => {
   return (
@@ -19,7 +20,7 @@ const Mypage = () => {
           <MyCard />
         </S.DisplayFlex>
       </div>
-      <div>최근 검색어</div>
+      <RightBanner />
     </S.MypageWrapper>
   );
 };
